77839a3a667b3bf0dd7428a8300b29b4601cb3a6
Alexander M <iammorjj@gmail.com>
2022-11-20 21:48:43 +0000

    Create compile_commands.json
    Use 'cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1':
    
    [
    {
      "directory": "/workspaces/try-codespace/build",
      "command": "/usr/bin/c++      -o CMakeFiles/Foo.dir/main.cpp.o -c /workspaces/try-codespace/main.cpp",
      "file": "/workspaces/try-codespace/main.cpp"
    },
    {
      "directory": "/workspaces/try-codespace/build",
      "command": "/usr/bin/c++      -o CMakeFiles/Foo.dir/foo.cpp.o -c /workspaces/try-codespace/foo.cpp",
      "file": "/workspaces/try-codespace/foo.cpp"
    },
    {
      "directory": "/workspaces/try-codespace/build",
      "command": "/usr/bin/c++      -o CMakeFiles/foo_copy.dir/foo_copy.cpp.o -c /workspaces/try-codespace/foo_copy.cpp",
      "file": "/workspaces/try-codespace/foo_copy.cpp"
    }
    ]

---
+++ build/CMakeFiles/Foo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Foo.dir/foo.cpp.o"
+  "CMakeFiles/Foo.dir/main.cpp.o"
+  "Foo"
+  "Foo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/foo_copy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foo_copy.dir/foo_copy.cpp.o"
+  "libfoo_copy.a"
+  "libfoo_copy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/foo_copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
